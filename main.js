091bed4d0e84054e923d223490f1aaf2c4996681
Romário Lima <devromariolima@gmail.com>
2024-02-26 18:20:33 -0300

    adição de Javascript

---
+++ main.js
@@ -0,0 +1,20 @@
+const $modal = document.getElementById('modal');
+const $descriptionInput = document.getElementById('description');
+const $priorityInput = document.getElementById('priority');
+const $deadLineInput = document.getElementById('deadLine');
+
+function openModal() {
+$modal.style.display = "flex";
+}
+
+function closeModal() {
+$modal.style.display = "none";
+}
+
+function creatTasks() {
+console.log($descriptionInput.value);
+console.log($priorityInput.value);
+console.log($deadLineInput.value);
+
+
+}
